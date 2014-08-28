27b4badb59d3e95e25be0d6c664e46c52cb20a15
tbemsi <bemsibom@aims.edu.gh>
2014-08-28 15:03:33 +0000

    We are looking at the easy way of doing things

--- hw.py
+++ hw.py
@@ -3,5 +3,6 @@ def main():
     print("Hello Ghana")
     print("This is a very interesting assignment")
     print("this is another test")
+    print("This was to see what happens the easy way")
 
 main()
