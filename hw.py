e2ec8a2c3e867c58ae5065fb17a60a7536b54787
Bemsibom Toh <tbemsi@gmail.com>
2018-12-14 12:48:39 +0000

    Delete hw.py

--- hw.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'bemsibom'
-def main():
-    print("Hello Ghana")
-    print("This is a very interesting assignment")
-    print("this is another test")
-    print("This was to see what happens the easy way")
-
-main()
